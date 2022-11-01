97807be550ddf9c4f265f0f41972d71423f5b20a
Maxi <emanuel-salas@hotmail.com>
2022-10-31 22:33:39 -0300

    req res

--- backend/Rutas/Router.js
+++ backend/Rutas/Router.js
@@ -20,7 +20,7 @@ Router.route("/home")
 
 Router.route("/formulario")
     .get(getForm)
-    .post(parseForm, csrfProtection, createForm)
+    .post(parseForm, csrfProtection, (req, res) => { createForm(req, res) })
 
 Router.route("/formulario/id")
     .get()
